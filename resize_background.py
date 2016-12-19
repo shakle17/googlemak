52434a1a3128823bf980d1414818951737659d79
lcadc17 <lcadc17@localhost.localdomain>
2016-12-20 00:46:37 +0100

    Updated gitingore and other

---
+++ resize_background.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from PIL import Image
+import os, sys
+
+path = "googlemak/game/images/backgrounds/"
+dirs = os.listdir( path )
+
+def resize():
+    for item in dirs:
+        if os.path.isfile(path+item):
+            im = Image.open(path+item)
+            f, e = os.path.splitext(path+item)
+            imResize = im.resize((1024,768), Image.ANTIALIAS)
+            imResize.save(f + '_done.png', 'PNG', quality=90)
+
+resize()
